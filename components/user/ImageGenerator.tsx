0f9e19297bd62773d38506de9a0d2b5a1df56371
harshkulkarni2024-pixel <harshkulkarni2024@gmail.com>
2025-10-05 18:28:22 +0330

    5687

---
+++ components/user/ImageGenerator.tsx
@@ -0,0 +1,8 @@
+// This component has been removed as the feature is no longer active.
+import React from 'react';
+
+const ImageGenerator: React.FC = () => {
+    return null;
+};
+
+export default ImageGenerator;
